c1f582cd1d7cb2d77ff377b93f854231ad0039f2
Syed Ahmed Ali <syedahmed4957@gmail.com>
2026-07-20 02:45:27 +0530

    Update gunicorn.conf.py

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -5,10 +5,12 @@ import os
 bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
 
 # Worker count (based on cpu count recommendations)
-workers = int(os.environ.get("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1))
+# workers = int(os.environ.get("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1))
+workers = int(os.getenv("GUNICORN_WORKERS", "2"))
 
 # Worker connection thread pool size
-threads = int(os.environ.get("GUNICORN_THREADS", 2))
+# threads = int(os.environ.get("GUNICORN_THREADS", 2))
+threads = int(os.getenv("GUNICORN_THREADS", "2"))
 
 # Maximum requests a worker will process before restarting (prevents memory leaks)
 max_requests = 1000
